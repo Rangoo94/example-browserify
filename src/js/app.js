fc811b34c72c1ae873e5c7df63370c8c009350c4
Dawid Rusnak <dawid@drcode.pl>
2015-03-22 13:57:30 +0100

    Prepared basic TabBox module

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
 (function() {
     'use strict';
+
+    // Dispatch all boxes with tabs
+    var TabBox = require('./modules/tab-box'),
+        tabs = document.getElementsByClassName('tabs');
+
+    for (var i = 0; i < tabs.length; i++) {
+        TabBox.create(tabs[i]);
+    }
 }());
---
+++ src/js/modules/tab-box.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    function TabBox(el) {
+    }
+
+    TabBox.create = function(el) {
+        return new TabBox(el);
+    };
+
+    module.exports = TabBox;
+}());
