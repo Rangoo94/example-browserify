7314d9d7d1ffd457b7b527176559c11acda6ad88
Dawid Rusnak <dawid@drcode.pl>
2015-03-26 14:53:59 +0100

    Added specs for `RSS`

--- test/spec/modules/rss.spec.js
+++ test/spec/modules/rss.spec.js
@@ -1,6 +1,31 @@
 (function() {
     'use strict';
 
-    // jshint unused: false
     var RSS = require('../../../src/js/modules/rss');
+
+    var fs = require('fs');
+    var rssFile = fs.readFileSync(__dirname + '/../../examples/rss-1.xml', 'utf8');
+
+    describe('RSS', function() {
+        it('should create instance of RSS', function() {
+            expect((new RSS(rssFile)) instanceof RSS).toBeTruthy();
+        });
+
+        it('should find items and transform them correctly', function() {
+            var rss = new RSS(rssFile),
+                items = rss.getItems();
+
+            expect(items.length).toEqual(10);
+
+            expect(items[0].pubdate instanceof Date).toBeTruthy();
+        });
+
+        it('should return basic variables from RSS', function() {
+            var rss = new RSS(rssFile);
+
+            expect(rss.get('title')).toEqual('VG RSS');
+            expect(rss.get('lastbuilddate') instanceof Date).toBeTruthy();
+            expect(rss.get('ttl')).toEqual(10);
+        });
+    });
 }());
