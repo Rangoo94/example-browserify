bbbb34962f87d9830f7a78dc71e84a7075f8fc92
Dawid Rusnak <dawid@drcode.pl>
2015-03-26 15:06:43 +0100

    Added basic specs for `ArticlesList`

--- test/spec/modules/articles-list.spec.js
+++ test/spec/modules/articles-list.spec.js
@@ -1,6 +1,31 @@
 (function() {
     'use strict';
 
-    // jshint unused: false
     var ArticlesList = require('../../../src/js/modules/articles-list');
+
+    function createItem(link, title, description) {
+        return {
+            link: link,
+            title: title,
+            description: description,
+            pubdate: new Date('Thu, 26 Mar 2015 12:53:14 +0100')
+        };
+    }
+
+    describe('ArticlesList', function() {
+        it('should create list from specified articles', function() {
+            var data = [
+                createItem('#', 'Title', 'Description'),
+                createItem('#', 'Title2'),
+                createItem('#', 'Title3', 'Description'),
+                createItem('#', 'Title4', 'Description'),
+                createItem('#', 'Title5', 'Description')
+            ], list = (new ArticlesList(data)).render();
+
+            expect(list instanceof Element).toBeTruthy();
+            expect(list.children.length).toEqual(5);
+            expect(list.children[0].children.length).toEqual(2);
+            expect(list.children[1].children.length).toEqual(1);
+        });
+    });
 }());
